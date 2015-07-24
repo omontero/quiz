b0b6b78b0593cadecf5e4b825c463580bbeeeb6b
omontero <oscar.montero.munoz@gmail.com>
2015-07-24 11:10:11 +0200

    Ejercicio modulo 8 v1.4

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -79,6 +79,7 @@ exports.edit = function(req, res) {
 exports.update = function(req, res) {
   req.quiz.pregunta  = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema = req.body.quiz.tema;
 
   var errors = req.quiz.validate();
   if (errors)
@@ -87,7 +88,7 @@ exports.update = function(req, res) {
   }
   else
   {
-        req.quiz     // save: guarda campos pregunta y respuesta en DB
+        req.quiz     // save: guarda campos pregunta, respuesta y tema en DB
         .save( {fields: ["pregunta", "respuesta", "tema"]})
         .then( function(){ res.redirect('/quizes');});
   }  
