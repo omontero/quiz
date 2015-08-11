dd2bb703f867302b058af5ee04fe044ae921a09c
omontero <oscar.montero.munoz@gmail.com>
2015-08-11 19:48:23 +0200

    Modulo 9 - Crear comentario

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,9 +2,15 @@ var models = require('../models/models.js');
 
 // Autoload :id
 exports.load = function(req, res, next, quizId) {
-models.Quiz.find(quizId).then(
-   function(quiz) {
-	     if (quiz) {
+models.Quiz.find({
+            where: {
+                id: Number(quizId)
+            },
+            include: [{
+                model: models.Comment
+            }]
+        }).then(function(quiz) {
+		     if (quiz) {
         req.quiz = quiz;
         next();
       } else{next(new Error('No existe quizId=' + quizId))}
